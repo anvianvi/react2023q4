d72fb144fada203d3ab2dc3b8094112f6832f957
Pavel <verbipo@gmail.com>
2023-11-05 18:57:37 +0100

    refactor:  refactor to hooks

--- src/SearchPage.tsx
+++ src/SearchPage.tsx
@@ -1,101 +1,83 @@
-import { Component } from 'react';
+import React, { useState, useEffect } from 'react';
 
 interface SearchResult {
   name: string;
   birth_year: string;
 }
 
-interface SearchPageState {
-  searchTerm: string;
-  searchResults: SearchResult[];
-  loading: boolean;
-}
-
 interface SearchPageProps {
-  someeNextProp: string;
+  someNextProp: string;
 }
 
-class SearchPage extends Component<
-  SearchPageProps | NonNullable<unknown>,
-  SearchPageState
-> {
-  constructor(props: SearchPageProps) {
-    super(props);
-    this.state = {
-      searchTerm: '',
-      searchResults: [],
-      loading: true,
-    };
-  }
+const SearchPage: React.FC<SearchPageProps> = () => {
+  const [searchTerm, setSearchTerm] = useState<string>('');
+  const [searchResults, setSearchResults] = useState<SearchResult[]>([]);
+  const [loading, setLoading] = useState<boolean>(true);
 
-  fetchData = async (term: string) => {
+  const fetchData = async (term: string) => {
     try {
-      this.setState({ loading: true });
+      setLoading(true);
       const response = await fetch(
         `https://swapi.dev/api/people/?search=${term}`
       );
       const data = await response.json();
-      this.setState({ searchResults: data.results });
+      setSearchResults(data.results);
     } catch (error) {
       console.error('Error fetching data:', error);
     } finally {
-      this.setState({ loading: false });
+      setLoading(false);
     }
   };
 
-  componentDidMount() {
+  useEffect(() => {
     const savedSearchTerm = localStorage.getItem('searchTerm');
 
     if (savedSearchTerm) {
-      this.setState({ searchTerm: savedSearchTerm.trim() });
-      this.fetchData(savedSearchTerm);
+      setSearchTerm(savedSearchTerm.trim());
+      fetchData(savedSearchTerm);
     }
-  }
+  }, []);
 
-  handleSearch = async () => {
+  const handleSearch = async () => {
     try {
-      await this.fetchData(this.state.searchTerm);
-      localStorage.setItem('searchTerm', this.state.searchTerm.trim());
+      await fetchData(searchTerm);
+      localStorage.setItem('searchTerm', searchTerm.trim());
     } catch (error) {
       console.error('Error during search:', error);
     }
   };
 
-  render() {
-    return (
-      <div>
-        <p style={{ maxWidth: '500px' }}>
-          Hi, it&apos;s my new styles solution. I have only 3 hours for this
-          task, and there&apos;s nothing in the instructions about styles - so
-          it&apos;s a perfect match. I use https://swapi.dev/api, and here you
-          can search for characters from Star Wars.
-        </p>
+  return (
+    <div>
+      <p style={{ maxWidth: '500px' }}>
+        Hi, it&apos;s my new styles solution. I have only 3 hours for this task,
+        and there&apos;s nothing in the instructions about styles - so it&apos;s
+        a perfect match. I use https://swapi.dev/api, and here you can search
+        for characters from Star Wars.
+      </p>
 
-        <div>
-          <input
-            type="text"
-            value={this.state.searchTerm}
-            onChange={(e) =>
-              this.setState({ searchTerm: e.target.value.trim() })
-            }
-          />
-          <button onClick={this.handleSearch}>Search</button>
-        </div>
-        <div>
-          {this.state.loading ? (
-            <p>Loading...</p>
-          ) : (
-            this.state.searchResults.map((result, index) => (
-              <div key={index}>
-                <h3>{result.name}</h3>
-                <p>{result.birth_year}</p>
-              </div>
-            ))
-          )}
-        </div>
+      <div>
+        <input
+          type="text"
+          value={searchTerm}
+          onChange={(e) => setSearchTerm(e.target.value.trim())}
+        />
+        <button onClick={handleSearch}>Search</button>
       </div>
-    );
-  }
-}
+      <div>
+        {loading ? (
+          <p>Loading...</p>
+        ) : (
+          searchResults.map((result, index) => (
+            <div key={index}>
+              <h3>{result.name}</h3>
+              <p>{result.birth_year}</p>
+            </div>
+          ))
+        )}
+      </div>
+    </div>
+  );
+};
 
 export default SearchPage;
